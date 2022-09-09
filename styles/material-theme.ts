f0ea95d3ec040d795ffc5b720ce22f052d341639
Tomas de Priede <tomasdepriede@gmail.com>
2022-09-09 13:56:27 -0300

    Fixed build issue

--- styles/material-theme.ts
+++ styles/material-theme.ts
@@ -2,7 +2,6 @@ import {createTheme} from "@mui/material";
 
 export const theme = createTheme({
     typography: {
-        fontFamily: 'Roboto, sans-serif',
-        letterSpacing: '.2rem',
+        fontFamily: 'Roboto, sans-serif'
     },
 });
